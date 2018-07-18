22cdf84f70c92b51ccb0b3f8b94f2178d68f0758
Josias NTUAREMBA <josiasdan@hotmail.fr>
2018-07-18 13:34:25 +0200

    tp tableau terminé

---
+++ 02.tableau.js
@@ -0,0 +1,6 @@
+var ville = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
+ville.forEach(function(element){console.log(element)});
+ console.log("lettreADansToutesLesVilles = "+ville.every(function(element){return element.includes('a')}))
+ console.log("auMoinsUneVilleAvecUnTiret = "+ville.some(function(element){return element.includes('-')}))
+ console.log("villesSansTiretSansEspace = "+ville.filter(function(element){return !element.includes('-')&&!element.includes(' ')}))
+ console.log("villesMajusculeSeTerminantParS = "+ville.filter(function(element){return element.endsWith('s')}).map(function(element){ return element.toUpperCase() }))
